69d340ac9f38d4760a2f10e02ac3459794a71038
Marcel Maqsood <marcel.maqsood@gmail.com>
2019-05-10 18:28:39 +0200

    Hinzufügen von render-Funktion für navigation Objekt; Ergänzung der navigation-Config.

--- config/navigation.global.php
+++ config/navigation.global.php
@@ -4,12 +4,44 @@ return [
     'depaNavigation' => [
         'navigations' => [
             'Application\depaNavigation\AppNavigation' => [
-
-
+                'test' => [
+                    'uri' => 'https://test1.de',
+                    'attributes' => [
+                        'id' => 'l',
+                        'class ' => 'Test_class'
+                    ],
+                    'childs' => [
+                        'subpath' => [
+                            'uri' => 'https://test2.de'
+                        ]
+                    ]
+                ],
+                'test2' => [
+                    'uri' => 'https://test2.de'
+                ]
             ],
+
             'Application\depaNavigation\AdminNavigation' => [
-
-
+                'admin1' => [
+                    'uri' => 'https://test1.de',
+                    'attributes' => [
+                        'id' => 'l',
+                        'class ' => 'Test_class'
+                    ],
+                    'childs' => [
+                        'subadmin' => [
+                            'uri' => 'https://test2.de',
+                            'childs' => [
+                                'subadmin' => [
+                                    'uri' => 'https://test2.de'
+                                ]
+                            ]
+                        ]
+                    ]
+                ],
+                'admin2' => [
+                    'uri' => 'https://test2.de'
+                ]
             ],
         ],
     ],
--- src/Navigation/Navigation.php
+++ src/Navigation/Navigation.php
@@ -4,6 +4,9 @@
 namespace depa\NavigationMiddleware\Navigation;
 
 
+use Knp\Menu\MenuFactory;
+use Knp\Menu\Renderer\ListRenderer;
+
 class Navigation
 {
     private $data;
@@ -11,5 +14,22 @@ class Navigation
     public function __construct($navigationData)
     {
         $this->data = $navigationData;
+    }
+
+    public function render(){
+        $factory = new MenuFactory();
+        $menu = $factory->createItem('My menu');
+        foreach($this->data as $key => $attributes){
+            $menu->addChild($key, $attributes);//Wie bringt man eine Navi unter, die childs, und childs von childs hat?
+            if(isset($attributes['childs'])){
+                foreach ($attributes['childs'] as $childKey => $child){
+                    $menu[$key]->addChild($childKey, $child);
+                }
+            }
+        }
+
+        $renderer = new ListRenderer(new \Knp\Menu\Matcher\Matcher());
+        echo $renderer->render($menu);
+        exit;
     }
 }
